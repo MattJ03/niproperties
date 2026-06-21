47958f0bca0cad3e5ac6afbc371132767d5c338a
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-21 23:05:49 +0100

    chore: set up more of the configurations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,12 @@
 import { createApp } from 'vue';
 import app from '../views/App.blade.php';
+import router from './router/index.js';
+import { createPinia } from 'pinia';
+
+const app = createApp(app);
+
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+app.mount('#app')
