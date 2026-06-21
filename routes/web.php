a04dcd5dad10273bc5ea1da09347afde016af2ae
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-21 23:51:51 +0100

    routes to point to vue

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('App');
-});
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '.*');
