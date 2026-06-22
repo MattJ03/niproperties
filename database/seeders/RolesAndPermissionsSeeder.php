2de460eaef2c91af22447839950ca782a69fdd30
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-22 16:13:42 +0100

    feat: added the roles and assigned roles to the landlord

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -3,11 +3,24 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder {
 
     public function run() {
+
+        $role1 = Role::create(['name' => 'landlord']);
+        $role2 = Role::create(['name' => 'buyer']);
+
+        $permission1 = Permission::create(['name' => 'list property']);
+        $permission2 = Permission::create(['name' => 'edit property']);
+        $permission3 = Permission::create(['name' => 'delete property']);
+        $permission4 = Permission::create(['name' => 'accept property offer']);
+        $permission5 = Permission::create(['name' => 'reject property offer']);
+        $permission6 = Permission::create(['name' => 'favourite property']);
+
+        $role1->givePermissionTo($permission1, $permission2, $permission3, $permission4, $permission5, $permission6);
 
     }
 }
