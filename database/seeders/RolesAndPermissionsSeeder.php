be68949edaa45907b9ab892d168098c5ac9a6f27
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-22 16:57:54 +0100

    feat: added permissions to buyer role

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -10,17 +10,18 @@ class RolesAndPermissionsSeeder extends Seeder {
 
     public function run() {
 
-        $role1 = Role::create(['name' => 'landlord']);
-        $role2 = Role::create(['name' => 'buyer']);
+        $role1 = Role::firstOrCreate(['name' => 'landlord']);
+        $role2 = Role::firstOrCreate(['name' => 'buyer']);
 
-        $permission1 = Permission::create(['name' => 'list property']);
-        $permission2 = Permission::create(['name' => 'edit property']);
-        $permission3 = Permission::create(['name' => 'delete property']);
-        $permission4 = Permission::create(['name' => 'accept property offer']);
-        $permission5 = Permission::create(['name' => 'reject property offer']);
-        $permission6 = Permission::create(['name' => 'favourite property']);
+        $permission1 = Permission::firstOrCreate(['name' => 'list property']);
+        $permission2 = Permission::firstOrCreate(['name' => 'edit property']);
+        $permission3 = Permission::firstOrCreate(['name' => 'delete property']);
+        $permission4 = Permission::firstOrCreate(['name' => 'accept property offer']);
+        $permission5 = Permission::firstOrCreate(['name' => 'reject property offer']);
+        $permission6 = Permission::firstOrCreate(['name' => 'favourite property']);
+        $permission7 = Permission::firstOrCreate(['name' => 'make an offer']);
 
-        $role1->givePermissionTo($permission1, $permission2, $permission3, $permission4, $permission5, $permission6);
-
+        $role1->givePermissionTo($permission1, $permission2, $permission3, $permission4, $permission5, $permission6, $permission7);
+        $role2->givePermissionTo($permission6, $permission7);
     }
 }
