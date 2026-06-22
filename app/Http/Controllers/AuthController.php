2194335ef979b510ff39f3f0ce71d94094e113bc
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-22 19:19:06 +0100

    feat: work on the authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function registerLandlord(Request $request) {
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'contact' => 'required|string|max:20',
+            'password' => 'required|string|min:8|confirmed',
+            ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'contact' => $validatedData['contact'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+    }
+}
