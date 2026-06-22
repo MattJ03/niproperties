979448e21671bc4c2795af25a298de676e8c8ae8
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-23 00:14:48 +0100

    feat: setting up the testing of authcontroller

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Http\Controllers\AuthController;
+use Database\Seeders\DatabaseSeeder;
 
 class AuthControllerTest extends TestCase
 {
@@ -17,5 +18,22 @@ class AuthControllerTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function __setUp(): void {
+        parent::setUp();
+        $this->seed(DatabaseSeeder::class);
+    }
+
+    public function test_register_landlord(): void {
+        $user = [
+            'name' => 'Test User',
+            'email' => 'real@email.com',
+            'contact' => '0123456789',
+            'password' => 'password',
+        ];
+
+        $response = $this->postJson('/api/registerLandlord', $user);
+        $response->assertStatus(201);
     }
 }
