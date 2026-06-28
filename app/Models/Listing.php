5be273151becd3f74fcb021dda9304f79fe60690
Matt Shaw <mattshaw2003@icloud.com>
2026-06-28 17:08:38 +0100

    fix: swapped from city to town for more specific filtering

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    protected $fillable = ['address_line_1', 'address_line_2', 'city', 'county', 'postcode', 'no_of_rooms', 'sale_status', 'latitude', 'longitude'];
+    protected $fillable = ['address_line_1', 'address_line_2', 'town', 'county', 'postcode', 'no_of_rooms', 'type', 'sale_status', 'latitude', 'longitude'];
 
 
     public function landlord() {
--- database/migrations/2026_06_27_192340_create_listings_table.php
+++ database/migrations/2026_06_27_192340_create_listings_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
-
+            $table->string('address_line_1');
+            $table->string('address_line_2');
+            $table->string()
             $table->timestamps();
         });
     }
