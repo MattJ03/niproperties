ef2a002845b1310ff3ec47417fafa86e5229223a
Matt Shaw <mattshaw2003@icloud.com>
2026-06-30 14:11:03 +0100

    feat: creating the geocoding ervice which makes an http request to the google geocoding api

--- app/Services/GeocodingService.php
+++ app/Services/GeocodingService.php
@@ -1,10 +1,25 @@
 <?php
 
+
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+use App\Models\Listing;
 class GeocodingService {
 
-    public function geocode($address) {
+    public function geocode(Listing $listing) {
+        $address = collect([
+            $listing->address_line_1,
+            $listing->address_line_2,
+            $listing->town,
+            $listing->county,
+            $listing->postcode,
+        ])->filter()->implode(',');
 
+        $result = Http::get('https://maps.googleapis.com/maps/api/geocode/json', [
+            'address' => $address . ' United Kingdom',
+            'key' => config('services.google.key'),
+            'components' => 'country:GB',
+        ]);
     }
 }
