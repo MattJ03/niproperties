e2250c87c414eed28e0e821ad115402f2ac34477
Matt Shaw <mattshaw2003@icloud.com>
2026-06-30 14:55:02 +0100

    chore: added the index endpoint to api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ListingController;
 
 Route::post('/registerLandlord', [AuthController::class, 'registerLandlord']);
 Route::post('/registerBuyer', [AuthController::class, 'registerBuyer']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/index', [ListingController::class, 'index']);
--- tests/Feature/ListingControllerTest.php
+++ tests/Feature/ListingControllerTest.php
@@ -8,11 +8,23 @@ use Tests\TestCase;
 
 class ListingControllerTest extends TestCase
 {
-
+     use RefreshDatabase;
     public function test_example(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->seed('DatabaseSeeder');
+    }
+
+    public function test_index_method_returns_200_status_code(): void {
+        $listings = Listing::factory()->count(10)->create();
+
+
+
     }
 }
