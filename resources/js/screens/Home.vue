a22e88ab16841736a6ee10caf06640ccd4b1794e
Matt Shaw <mattshaw2003@icloud.com>
2026-07-02 17:35:37 +0100

    feat: first draft, gonna add nav bar first and then see where we are screen sizing wise

--- resources/js/screens/Home.vue
+++ resources/js/screens/Home.vue
@@ -5,10 +5,22 @@
             <div class="hello-user-wrapper">
                 <div class="hello-row">
                 <h2 class="hello-message">Find your new home</h2>
-                <h2 class="hello-message-name" v-if="name"> {{ name }}</h2>
+                    <h2 class="hello-message-name" v-if="name"> {{ name }}</h2>
+                </div>
+                    <div class="type-of-home-selection">
+                        <div class="selection-background">
+                            <strong><span>Buy</span></strong>
+                        </div>
+                        <div class="selection-background">
+                            <strong><span>Rent</span></strong>
+                        </div>
+                        <div class="selection-background">
+                            <strong><span>Commercial</span></strong>
+                        </div>
                 </div>
                 <div class="search-wrapper">
-                    <input type="text" v-model="search" class="search-bar"/>
+                    <input type="text" v-model="search" class="search-bar" placeholder="what are you looking for..."/>
+                    <button class="search-btn">Search</button>
                 </div>
             </div>
 
@@ -57,12 +69,14 @@ const search = ref('');
     display: flex;
     flex-direction: column;
     margin: auto;
-    width: 30%;
+    width: 35%;
+    padding-bottom: 70px;
 }
 .hello-message {
     color: #FFFFFF;
     z-index: 1;
     position: relative;
+    flex-direction: row;
 }
 .hello-message-name {
     z-index: 1;
@@ -76,15 +90,68 @@ const search = ref('');
     align-items: center;
     flex-direction: row;
 }
+.type-of-home-selection {
+    display: flex;
+    z-index: 1;
+    position: relative;
+   justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+    font-size: 20px;
+    gap: 50px;
+    padding-top: 30px
+}
+.type-of-home-selection span {
+    color: #FFFFFF;
+}
+.selection-background {
+
+}
+.selection-background:hover {
+    border-bottom: 5px solid #1F4D3A;
+    transition: 0.3s;
+    cursor: pointer;
+}
 .search-wrapper {
+    display: flex;
+    flex-direction: row;
     z-index: 1;
     position: relative;
     height: 50px;
+    padding-top: 30px;
+    margin-right: 0;
 }
 .search-bar {
     position: relative;
     z-index: 1;
-    height: 50px;
+    height: 66px;
     width: 100%;
+    border-radius: 30px 0px 0px 30px;
+    border: none;
+    padding-left: 16px;
+    font-size: 16px;
+}
+.search-bar:hover {
+    background-color: #F2EFE6;
+    cursor: pointer;
+}
+.search-btn {
+    display: flex;
+    z-index: 1;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    height: 66px;
+    background-color: #1F4D3A;
+    color: #FFFFFF;
+    font-size: 16px;
+    width: 15%;
+    cursor: pointer;
+    border-radius: 0px 30px 30px 0px;
+    padding-right: 20px;
+    border: none;
+}
+.search-btn:hover {
+    background-color:  #2d6e53;
 }
 </style>
