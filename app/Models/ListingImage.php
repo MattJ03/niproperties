502f59f16a175092f02aaa7441d2d2d811b4a6ad
Matt Shaw <mattshaw2003@icloud.com>
2026-07-04 18:09:26 +0100

    feat: set up the model file for the ListingImage

--- app/Models/ListingImage.php
+++ app/Models/ListingImage.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ListingImage extends Model
 {
-    //
+    protected $fillable = ['listing_id', 'file_path', 'file_type', 'user_id', 'landlord_id'];
+
+    public function listing() {
+        return $this->belongsTo(Listing::class);
+    }
 }
