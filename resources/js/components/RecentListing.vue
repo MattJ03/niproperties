5447f8f96d2574d428fc74b6dc9759d329e03c74
Matt Shaw <mattshaw2003@icloud.com>
2026-07-31 18:00:27 +0100

    feat: check to make sure there is a primary image if not use index 0

---
+++ resources/js/components/RecentListing.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="img-wrapper">
+
+        </div>
+
+    </div>
+
+</template>
+<script setup>
+import { ref, reactive, computed, onMounted } from 'vue';
+
+
+const props = defineProps({
+    listing: {
+        type: Object,
+        required: true,
+    },
+});
+
+const primaryImageCheck = computed(() => {
+    if(props.listing.listing.listing_images === null) return null;
+    return props.listing.listing.listing_images.find(img => img.is_primary) ?? props.listing.listing_images[0];
+})
+</script>
+<style scoped>
+.container {
+    display: flex;
+
+}
+.img-wrapper {
+    display: flex;
+    height: 100%;
+    width: 100%;
+}
+</style>
