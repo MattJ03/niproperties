75126c7484953a2003c107cae77454988510d87c
Matt Shaw <mattshaw2003@icloud.com>
2026-08-19 16:11:47 +0100

    feat: created the UserDirectoryStore

--- resources/js/screens/OurLandlords.vue
+++ resources/js/screens/OurLandlords.vue
@@ -1,7 +1,16 @@
 <template>
+    <Navbar></Navbar>
+    <div class="container">
 
+    </div>
 </template>
 <script setup>
+import { ref, reactive, computed, onMounted } from 'vue';
+import Navbar from "../components/Navbar.vue";
 </script>
 <style scoped>
+.container {
+    display: flex;
+    width: 100%;
+}
 </style>
