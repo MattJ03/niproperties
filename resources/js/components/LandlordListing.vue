a2bd75dfec015678bee553c523c3a3272558987f
Matt Shaw <mattshaw2003@icloud.com>
2026-09-01 16:52:58 +0100

    feat: line to get the primary image

--- resources/js/components/LandlordListing.vue
+++ resources/js/components/LandlordListing.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <div class="img-wrapper">
+            <img :src="`/api/listings/listing-images/${primaryImage.id}`" class="listing-img" alt="listing image" />
+        </div>
 
     </div>
 
@@ -7,6 +10,8 @@
 <script setup>
 import { ref, reactive, computed } from 'vue';
 
+
+const noImage = ref('')
 const props = defineProps({
     listing: {
         type: Object,
@@ -14,11 +19,29 @@ const props = defineProps({
     },
 });
 
+const primaryImage = computed(() => {
+    if(props.listing.listing_images === null || props.listing.listing_images === undefined) {
+        noImage.value = 'no listing images';
+        return null;
+    }
+    return props.listing.listing.listing_images.find(img => img.isPrimary) ?? props.listing.listing_images[0];
+})
 
 </script>
 <style scoped>
 .container {
     display: flex;
     width: 100%;
+    height: 25dvh;
+}
+.img-wrapper {
+    overflow: hidden;
+    height: 40%;
+    width: 100%;
+}
+.listing-img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
 }
 </style>
