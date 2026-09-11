9f4b5731b4138a62eeb48da60d8752aba589889e
Matt Shaw <mattshaw2003@icloud.com>
2026-09-11 22:55:59 +0100

    fix: refactored the average rent price method to take advantage of database calcualtions rather than the for loop

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -629,18 +629,12 @@ class ListingController extends Controller
     }
 
     public function getAverageRentPrice() {
-      $sum = '';
       $rentals = Listing::where('sale_status', 'open')
-                          ->where('type', 'rent')
-                           ->where('sale_status', 'open')
-                           ->get();
-      foreach($rentals as $rental) {
-          $sum = $rental->rent_per_mont;
-      }
-      $total = $sum / $rentals->count();
+                           ->where('type', 'rent')
+                           ->avg('rent_per_month');
 
       return response()->json([
-          'average_rent' => $total,
-      ]);
-  }
+          'average_rent' => round($rentals, 2),
+          ]);
+    }
 }
