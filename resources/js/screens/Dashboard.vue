f9ddf57f2db03298476249ec65d5afd13fe2eefc
Matt Shaw <mattshaw2003@icloud.com>
2026-09-11 15:17:35 +0100

    feat: more design made to the dashboard square

--- resources/js/screens/Dashboard.vue
+++ resources/js/screens/Dashboard.vue
@@ -1,7 +1,11 @@
 <template>
     <Navbar></Navbar>
     <div class="container">
-
+        <div class="row-of-stats">
+            <div class="analytics-square">
+                <span class="analytics-square-title">Total listings</span>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
@@ -21,5 +25,29 @@ const userStore = useUserDirectoryStore();
 .container {
     display: flex;
     width: 100%;
+}
+.row-of-stats {
+    display: flex;
+    flex-direction: row;
+    height: 15dvh;
+    width: 100%;
+    background-color: #FFFFFF;
+    margin-top: 150px;
+    padding-left: 40px;
+}
+.analytics-square {
+    display: flex;
+    flex-direction: column;
+    border-radius: 14px;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+    height: 150px;
+    width: 200px;
+    border: 1px solid #000000;
+}
+.analytics-square-title {
+    margin-left: 15px;
+    margin-top: 10px;
+    color: #88807b;
+    font-size: 18px;
 }
 </style>
