09a21055907412fcccaa5b5a28a1139acf1cb0c2
Matt Shaw <mattshaw2003@icloud.com>
2026-09-11 16:40:21 +0100

    feat: test case to ensure method works

--- tests/Feature/UserDirectoryControllerTest.php
+++ tests/Feature/UserDirectoryControllerTest.php
@@ -2,19 +2,35 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Spatie\Permission\Models\Role;
+
 
 class UserDirectoryControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    public function setUp(): void{
+        parent::setUp();
+        $this->seed(DatabaseSeeder::class);
+    }
     public function test_example(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_get_users_returns_all_users(): void {
+        $users = User::factory()->count(5)->create();
+
+        $response = $this->getJson('/api/getUsers');
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'users_count' => 5,
+        ]);
     }
 }
